5cdcb4858259a12b8a0525ee042ef523424f1e83
Pragith Prakash <pragith@gmail.com>
2014-02-07 18:40:35 +0530

    One liners added.

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,12 +1,19 @@
 #	FizzBuzz
 #	https://www.hackerrank.com/challenges/fizzbuzz
 
-for n in range(1,101):
-	if (n % 3 == 0 and n % 5 != 0):
-		print "Fizz"
-	elif (n % 5 == 0 and n % 3 != 0):
-		print "Buzz"
-	elif (n % 3 == 0 and n % 5 == 0):
-		print "FizzBuzz"
-	else:
-		print n
+for i in range(101):
+    if i==0:
+        continue
+    elif i%15==0:
+        print 'FizzBuzz'
+    elif i%5==0:
+        print 'Buzz'
+    elif i%3==0:
+        print 'Fizz'
+    else:
+        print i
+
+# for i in range(1,101):print"Fizz"*(i%3<1)+"Buzz"*(i%5<1)or i
+# for x in range(100): print x%3/2*'Fizz'+x%5/4*'Buzz' or x+1
+# print '\n'.join('Fizz'*(not i%3) + 'Buzz'*(not i%5) or str(i) for i in range(1, 101))
+# for i in range(1,101):print"FizzBuzz"[i*i%3*4:8--i**4%5]or i
