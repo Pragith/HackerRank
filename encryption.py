25d5dad2e2671ee75b8f6e566bf53af2cefd8ac2
Pragith Prakash <pragith@gmail.com>
2014-02-13 18:39:55 +0530

    WIP.

--- encryption.py
+++ encryption.py
@@ -1,6 +1,33 @@
 #	Encryption
 #	https://www.hackerrank.com/challenges/encryption
 
+import math
+
+msg = "chillout"
 msg = "feedthedog"
 msg = "haveaniceday"
 
+length = len(msg)
+
+width = int(math.sqrt(length))
+height = int(math.ceil(math.sqrt(length)))
+
+if (width < height):
+	rows = width
+	cols = height
+else:
+	rows = height
+	cols = width
+
+print "Rows:",rows,"Columns:",cols
+
+
+
+
+i = 0
+
+for m in msg:
+	if (i % cols == 0):
+		print ''
+	print m,
+	i+=1	
