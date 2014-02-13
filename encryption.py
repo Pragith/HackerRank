5ead9e3f8b8e9faf2a01b006c7fd5c7b043e988c
Pragith <git@pragith.net>
2014-02-13 23:27:41 +0530

    WIP - a little closer

--- encryption.py
+++ encryption.py
@@ -4,8 +4,8 @@
 import math
 
 msg = "chillout"
-msg = "feedthedog"
-msg = "haveaniceday"
+#msg = "feedthedog"
+#msg = "haveaniceday"
 
 length = len(msg)
 
@@ -23,11 +23,63 @@ print "Rows:",rows,"Columns:",cols
 
 
 
+def split_len(seq, length):
+	return [seq[i:i+length] for i in range(0, len(seq), length)]
 
 i = 0
+arr = []
+
+seq = 'chillout'
+length = cols
+
+for i in range(0, len(seq), length):
+	print i,i+length,
+	print seq[i:i+length]
+
+for chunk in split_len(msg,cols):
+	print ''
+	#print chunk[1],
+
+
+
+
+
+'''
+
+
+i = 0
+
 
 for m in msg:
 	if (i % cols == 0):
 		print ''
+	print i,
+	i += 1
+
+print arr
+
+
+
+
+
+
+
+
+
+i = 0
+arr = []
+
+for m in msg:
+	if (i % cols == 0):
+		print ''
+		arr.append('')
 	print m,
+	arr.append(m)
 	i+=1	
+
+print arr
+
+
+'''
+
+
